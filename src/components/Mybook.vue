24069a1d4903d3ec4df2cdb0887a72555bdb612e
coolkidneversleep <iiippbhy@gmail.com>
2021-04-11 21:31:21 +0700

    add alert box in mybooking page

--- src/components/Mybook.vue
+++ src/components/Mybook.vue
@@ -10,7 +10,7 @@
           <v-row style="padding-bottom:0px">
             <v-col cols="12" sm="12" style="padding-bottom:0px">
               <v-text-field
-              v-model="booking_id"
+                v-model="booking_id"
                 label="Your Booking ID"
                 background-color="#F0F2FF"
                 filled
@@ -25,7 +25,7 @@
           <v-row style="padding-bottom:0px">
             <v-col cols="12" sm="12" style="padding-bottom:0px">
               <v-file-input
-              v-model="image"
+                v-model="image"
                 label="File input"
                 filled
                 rounded
@@ -38,19 +38,39 @@
           <v-row
             style="padding-buuton:70px; padding-top:0px; padding-left:140px"
           >
-            <v-btn
-              rounded
-              dark
-              large
-              elevation="2"
-              color="#7586F7"
-              width="150px"
-              height="50px"
-              style="text-transform:Capitalize"
-              @click="verifyBooking"
-            >
-              Upload
-            </v-btn>
+            <v-dialog v-model="dialog" persistent max-width="290">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  rounded
+                  dark
+                  large
+                  elevation="2"
+                  color="#7586F7"
+                  width="150px"
+                  height="50px"
+                  style="text-transform:Capitalize"
+                  @click="verifyBooking"
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  Upload
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title class="headline">
+                  Approve Success !
+                </v-card-title>
+                <v-card-text
+                  >Thank you for your interesting!</v-card-text
+                >
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="green darken-1" text @click="dialog = false">
+                    ok
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
           </v-row>
         </v-card>
       </v-col>
@@ -62,30 +82,34 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 // import axios from 'axios';
 export default {
   name: "Mybook",
   components: {},
 
-  data: ()=> ({
-      booking_id: "",
-      image: null,
-      
+  data: () => ({
+    booking_id: "",
+    image: null,
+    dialog: false,
   }),
   methods: {
-    verifyBooking(){
-      const formData = new FormData()
-      formData.append('booking_id',this.booking_id)
-      this.image.filename = this.booking_id
-      formData.append('images',this.image)
-      axios.post('http://localhost:3000/slip',formData,{headers: {
-      'Content-Type': 'multipart/form-data'
-    }}).then(()=>{
-        console.log("Success")
-      })
-    }
-  }
+    verifyBooking() {
+      const formData = new FormData();
+      formData.append("booking_id", this.booking_id);
+      this.image.filename = this.booking_id;
+      formData.append("images", this.image);
+      axios
+        .post("http://localhost:3000/slip", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then(() => {
+          console.log("Success");
+        });
+    },
+  },
 };
 </script>
 <style scoped>
